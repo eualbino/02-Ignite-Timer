9377d16cd4d162882752fad59fb215a06070cd96
Lucas Apóstolo <lucas.g.apostolo@gmail.com>
2023-07-27 10:37:00 -0300

    Ignite Timer

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer, ButtonVariant } from './Button.styles.ts';
+
+interface ButtonProps{
+    variant?: ButtonVariant;
+}
+
+const Button = ({ variant = 'primary' }: ButtonProps) => {
+    return ( 
+        <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+    );
+}
+
+export default Button;
